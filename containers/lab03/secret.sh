4aa971d2452c813b85e2de9a1ab6ff4b229260cf
Robert Moon <moo7596@s286657.nm.nmfco.com>
2019-02-19 15:38:59 -0600

    base64 first

--- containers/lab03/secret.sh
+++ containers/lab03/secret.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-db_secret=$1
+db_secret=$(echo $1 | base64)
 
 if [[ -z $db_secret ]]; then
     echo 'please provide a secret after calling this command'
     exit 1
 fi
-
-$db_secret_64="$(echo $db_secret | base64)"
-echo $db_secret_64
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -17,5 +14,5 @@ metadata:
   name: dbpass
 type: Opaque
 data:
-  dbpass: ${db_secret_64}
+  dbpass: ${db_secret}
 EOF
